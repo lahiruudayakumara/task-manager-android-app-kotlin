986d2783a8393b7d06cb4db87fb22d8228a671a2
LAHIRU <lahiruudayakumara950@gmail.com>
2024-05-11 23:17:32 +0530

    Add constant values for task table columns

---
+++ app/src/main/java/com/example/taskmanagerapp/utils/Constant.kt
@@ -0,0 +1,10 @@
+package com.example.taskmanagerapp.utils
+
+
+const val TABLE_NAME = "TaskTable"
+const val COLUMN_NAME_TITLE = "TaskTitle"
+const val COLUMN_NAME_DESCRIPTION = "TaskDescription"
+const val COLUMN_NAME_DATE = "TaskDate"
+const val COLUMN_NAME_TIME = "TaskTime"
+const val COLUMN_NAME_PRIORITY = "TaskPriority"
+const val COLUMN_NAME_STATUS = "TaskStatus"
