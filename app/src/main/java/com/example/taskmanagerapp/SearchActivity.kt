0139c9929467242f4f1bc167672dabf3aa850aa8
LAHIRU <lahiruudayakumara950@gmail.com>
2024-05-12 23:02:00 +0530

    Add date picker functionality to SearchActivity

--- app/src/main/java/com/example/taskmanagerapp/SearchActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/SearchActivity.kt
@@ -1,8 +1,12 @@
 package com.example.taskmanagerapp
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,11 +21,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var searchBcack: ImageButton
     private lateinit var searchRecyclerView: RecyclerView
+    private lateinit var calendar: Calendar
+    private lateinit var btnPickDate:Button
+
+    private lateinit var dateTextView: TextView
+
+    private var selectedDate: String = ""
 
     private val dbOpenHelper by lazy { DBOpenHelper(this) }
     private lateinit var job: Job
@@ -34,16 +46,41 @@ class SearchActivity : AppCompatActivity() {
         searchBcack = findViewById(R.id.fab_search_back_main)
         searchRecyclerView = findViewById(R.id.search_recycler_view)
 
+        calendar = Calendar.getInstance()
+        selectedDate = SimpleDateFormat("yyyy-MM-dd").format(calendar.time)
+        updateDateTextView()
+
         setupRecyclerView(searchRecyclerView)
 
         job = CoroutineScope(Dispatchers.Main).launch {
-            searchRecyclerView.adapter = TaskAdapter(this@SearchActivity, fetchTasksFromDatabase("In Progress"))
+            searchRecyclerView.adapter = TaskAdapter(this@SearchActivity, fetchTasksFromDatabase("$selectedDate"))
         }
 
         searchBcack.setOnClickListener {
             searchBackToMain()
         }
 
+        btnPickDate = findViewById(R.id.btn_pick_date)
+        btnPickDate.setOnClickListener {
+            showDatePicker()
+        }
+
+    }
+
+    private fun showDatePicker() {
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year, monthOfYear, dayOfMonth ->
+                calendar.set(year, monthOfYear, dayOfMonth)
+                selectedDate = SimpleDateFormat("yyyy-MM-dd").format(calendar.time)
+                updateDateTextView()
+                updateRecyclerView(selectedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
     }
 
     private fun searchBackToMain() {
@@ -67,9 +104,22 @@ class SearchActivity : AppCompatActivity() {
     }
 
 
-    private suspend fun fetchTasksFromDatabase(status: String): MutableList<TaskModel> {
+    private suspend fun fetchTasksFromDatabase(date: String): MutableList<TaskModel> {
         return withContext(Dispatchers.IO) {
-            dbOpenHelper.readTaskByStatus(status)
+            dbOpenHelper.readTaskByOnlyDate(date)
         }
     }
+
+    private fun updateDateTextView() {
+        dateTextView = findViewById(R.id.display_data)
+        dateTextView.text = "$selectedDate"
+    }
+
+    private fun updateRecyclerView(selectedDate: String) {
+        job = CoroutineScope(Dispatchers.Main).launch {
+            searchRecyclerView.adapter = TaskAdapter(this@SearchActivity, fetchTasksFromDatabase(selectedDate))
+        }
+    }
+
+
 }
